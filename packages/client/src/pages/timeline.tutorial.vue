bb8cac1a218ba25590639f38598dd356a42b77a4
Kevgen <87700221+Evgen0922@users.noreply.github.com>
2022-11-05 22:14:36 +0300

    fix | 0.0.98-beta

--- packages/client/src/pages/timeline.tutorial.vue
+++ packages/client/src/pages/timeline.tutorial.vue
@@ -73,62 +73,8 @@
 		<div>{{ i18n.ts._tutorial.step1_2 }}</div>
 		<div>{{ i18n.ts._tutorial.step1_3 }}</div>
 		<div>{{ i18n.ts._tutorial.step1_4 }}</div>
-	</div>
-	<!-- <div v-else-if="tutorial === 1" class="_content">
-		<div>{{ i18n.ts._tutorial.step2_1 }}</div>
-		<div>{{ i18n.ts._tutorial.step2_2 }}</div>
-		<MkA class="_link" to="/settings/profile">{{ i18n.ts.editProfile }}</MkA>
-	</div>
-	<div v-else-if="tutorial === 2" class="_content">
-		<div>{{ i18n.ts._tutorial.step3_1 }}</div>
-		<div>{{ i18n.ts._tutorial.step3_2 }}</div>
-		<div>{{ i18n.ts._tutorial.step3_3 }}</div>
-		<small>{{ i18n.ts._tutorial.step3_4 }}</small>
-	</div>
-	<div v-else-if="tutorial === 3" class="_content">
-		<div>{{ i18n.ts._tutorial.step4_1 }}</div>
-		<div>{{ i18n.ts._tutorial.step4_2 }}</div>
-	</div>
-	<div v-else-if="tutorial === 4" class="_content">
-		<div>{{ i18n.ts._tutorial.step5_1 }}</div>
-		<I18n :src="i18n.ts._tutorial.step5_2" tag="div">
-			<template #featured>
-				<MkA class="_link" to="/featured">{{ i18n.ts.featured }}</MkA>
-			</template>
-			<template #explore>
-				<MkA class="_link" to="/explore">{{ i18n.ts.explore }}</MkA>
-			</template>
-		</I18n>
-		<div>{{ i18n.ts._tutorial.step5_3 }}</div>
-		<small>{{ i18n.ts._tutorial.step5_4 }}</small>
-	</div>
-	<div v-else-if="tutorial === 5" class="_content">
-		<div>{{ i18n.ts._tutorial.step6_1 }}</div>
-		<div>{{ i18n.ts._tutorial.step6_2 }}</div>
-		<div>{{ i18n.ts._tutorial.step6_3 }}</div>
-	</div>
-	<div v-else-if="tutorial === 6" class="_content">
-		<div>{{ i18n.ts._tutorial.step7_1 }}</div>
-		<I18n :src="i18n.ts._tutorial.step7_2" tag="div">
-			<template #help>
-				<a href="https://github.com/Evgen0922/speechka-dev_ynh" target="_blank" class="_link">{{ i18n.ts.help }}</a>
-			</template>
-		</I18n>
-		<div>{{ i18n.ts._tutorial.step7_3 }}</div>
-	</div>
-
-	<div class="_footer navigation">
-		<div class="step">
-			<button class="arrow _button" :disabled="tutorial === 0" @click="tutorial--">
-				<i class="fas fa-chevron-left"></i>
-			</button>
-			<span>{{ tutorial + 1 }} / 7</span>
-			<button class="arrow _button" :disabled="tutorial === 6" @click="tutorial++">
-				<i class="fas fa-chevron-right"></i>
-			</button>
-		</div> -->
+	 </div>
 		<MkButton v-if="tutorial === 0" class="ok" primary @click="tutorial = -1"><i class="fas fa-check"></i> {{ i18n.ts.gotIt }}</MkButton>
-	</div>
 </div>
 </template>
 
